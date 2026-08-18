4991b9531de0c9e093f0e3dbdb20ccd3ea6cb7e4
Luiz Andre Fernandes Pego <luizandreafonsopego@gmail.com>
2026-08-18 14:35:19 -0300

    Aula inicial

---
+++ Aula 1/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//Criando um tipo que representa um no
+//(Caixinha)
+
+struct Node
+{
+    int X; //X e o dado manipulado pela pilha
+    Node *next; //*next e um ponteiro que guarda o endereço da proxima caixinha
+
+};
+
+
+
+//Aqui estou criando uma classe:
+//Que e um novo tipo de dados que possui dados e funcoes
+//Os dados(atributos) e funcoes(metodos)
+
+class Stack
+{
+private:
+    Node *top;
+    int n;
+public:
+
+
+
+};
+//Objeto é a variavel da classe
+int main()
+{
+
+}
+
